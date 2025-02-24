520ae9277a889d8db77ebaf1ab4a52aaccbcdda1
Mohammed Zaheen <mzaheen@Mohammeds-MacBook-Air.local>
2025-02-24 21:14:43 +0530

    Setting up portfolio project.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Tailwind CSS is working! 🚀</h1>
+    </div>
+  );
+}
+
+export default App
